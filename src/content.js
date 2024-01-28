3fb5a11e0d907bd7cfec33c20f2b9a42bbe0e119
yoCkawa0 <18nc047@ms.dendai.ac.jp>
2024-01-29 01:51:24 +0900

    Update Chrome extension source files
    This commit includes updates to several files in our Chrome extension. Changes include modifications to the content script, manifest file, popup HTML, and popup JavaScript. These updates are intended to improve the extension's performance and user interface.

--- src/content.js
+++ src/content.js
@@ -1,4 +1,10 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    let title = document.querySelector('div[style="line-height: 1.5; word-break: break-word; white-space: pre-wrap; pointer-events: none;"]').textContent
-    sendResponse(title);
+  if (request.message === "getDomValue") {
+    let divElement = document.querySelector(
+      'div[style="line-height: 1.5; word-break: break-word; white-space: pre-wrap; display: flex; flex-wrap: wrap;"]'
+    );
+    let spanElement = divElement.querySelector("span");
+    let textContent = spanElement.textContent;
+    chrome.runtime.sendMessage({ message: "domValue", content: textContent });
+  }
 });
--- src/popup.js
+++ src/popup.js
@@ -1,40 +1,44 @@
-const task_id = document.getElementById("taskId");
-let google_url = "your google apps scripts url";
-let origin_id = '';
-const regex = /\bp=([a-f0-9]+)\b/;
-
-chrome.tabs.query( {active:true, currentWindow:true}, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, {message: 'getname'}, (content) => {
-        if(!content){
-            alert('Cannot Get! Try Reload First!');
-            return;
-        }
-        document.getElementById('taskId').value = content
-    });
+document.addEventListener("DOMContentLoaded", (event) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { message: "getDomValue" });
+  });
 });
 
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === "domValue") {
+    document.getElementById("taskId").value = request.content;
+  }
+});
 
-document.getElementById("btn").onclick = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        let url = tabs[0].url;
-        let dashIndex = url.lastIndexOf("-");
-        if(url.match(regex)){
-            origin_id = url.match(regex)[1];
-        }else if (dashIndex === -1) {
-            dashIndex = url.lastIndexOf("/");
-            origin_id = url.substring(dashIndex + 1);
-        }else{
-            origin_id = url.substring(dashIndex + 1);
-        }
+let google_url = "";
+let origin_id = "";
+const regex = /\bp=([a-f0-9]+)\b/;
 
-        // create request url
-        google_url += "?originId=" + origin_id + "&taskId=" + task_id.value;
-        alert(google_url);
-
-        // send request
-        fetch(google_url, {
-            method: "GET",
-            mode: "cors"
-        });
+document.addEventListener("DOMContentLoaded", () => {
+  const task_id = document.getElementById("taskId");
+  const actionButton = document.getElementById("btn");
+  actionButton.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      let url = tabs[0].url;
+      let dashIndex = url.lastIndexOf("-");
+      if (url.match(regex)) {
+        origin_id = url.match(regex)[1];
+      } else if (dashIndex === -1) {
+        dashIndex = url.lastIndexOf("/");
+        origin_id = url.substring(dashIndex + 1);
+      } else {
+        origin_id = url.substring(dashIndex + 1);
+      }
+      // create request url
+      google_url += "?originId=" + origin_id + "&taskId=" + task_id.value;
+      alert(google_url);
+      // send request
+      fetch(google_url, {
+        method: "GET",
+        mode: "cors",
+      }).catch((error) => {
+        console.error(error);
+      });
     });
-};
+  });
+});
